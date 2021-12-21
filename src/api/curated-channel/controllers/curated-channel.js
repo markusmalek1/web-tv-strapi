91b061212c564456c3da4d8242ecdc32244f0a06
Malek <markusmalek1@gmail.com>
2021-12-21 16:45:51 +0100

    fix error on add curated

--- src/api/curated-channel/controllers/curated-channel.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  curated-channel controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::curated-channel.curated-channel');
--- src/api/curated-channel/services/curated-channel.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * curated-channel service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::curated-channel.curated-channel');
